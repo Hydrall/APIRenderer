579aa4d47da67c1c15034529f73b07302041abac
Jacob Coleman <Jacob.Coleman@leidos.com>
2018-05-25 11:58:58 -0400

    Fixing up the twitter templates, moving all inline styles to CSS because it should be.

--- tpl/twitter_feed.tpl.php
+++ tpl/twitter_feed.tpl.php
@@ -9,7 +9,11 @@
  */
 ?>
 <div class="col-md-12">
-<h1 style="display:inline">Tweets</h1><span>  by @<?php print $screen_name ?></span>
-	<?php print $content ?>
-  <a href="#" class="btn btn-default">Follow me on twitter </a>
+  <div class="twitter-title">
+    <h1 class="twitter-title-1">Tweets</h1><span class="twitter-title-2">  by @<?php print $screen_name ?></span>
+  </div>
+	<div class="twitter-content">
+    <?php print $content ?>
+  </div>
+  <a href="#" class="twitter-follow btn btn-default">Follow me on twitter </a>
 </div>
